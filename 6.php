68fbeca5838977d9187b705e2e294df36c60fa37
Yuki03280322 <my-op.cpace@outlook.jp>
2021-04-12 17:11:22 +0900

    Create 6.php

---
+++ 6.php
@@ -0,0 +1,28 @@
+<?php
+function count_who($str) {
+  $result = preg_match("/who/i", $str);
+  if ($result) {
+    echo "マッチしました\n";
+  } else {
+    echo "マッチしませんでした\n";
+  }
+}
+
+count_who("whoareyou");
+count_who("whereareyoufrom?");
+count_who("Whoareyouwhowho");
+
+function count_what($char) {
+  $result = preg_match_all("/what/", $char);
+  if ($result) {
+    echo $result."件マッチしました\n";
+  } else {
+    echo "マッチしませんでした\n";
+  }
+}
+
+count_what("whatwhatwho");
+count_what("whywhowhere");
+count_what("WhatWhatwhat")
+
+?>
