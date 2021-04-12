ed3dc3c61f40e1d93a5969417d2fd78a048d9169
Yuki03280322 <my-op.cpace@outlook.jp>
2021-04-12 12:43:59 +0900

    Create 5.php

---
+++ 5.php
@@ -0,0 +1,16 @@
+<?php
+function police_trouble($a,$b) {
+  if (($a == true && $b == true) || ($a == false && $b == false)) {
+    echo "True\n";
+  } else {
+    echo "False\n";
+  }
+}
+
+police_trouble(false, false);
+police_trouble(true, false);
+police_trouble(false, false);
+police_trouble(false, true);
+police_trouble(true, true);
+
+?>
