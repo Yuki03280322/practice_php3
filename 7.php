40841165f68fc01690fb46d284d796785683cb53
Yuki03280322 <my-op.cpace@outlook.jp>
2021-04-13 00:10:32 +0900

    Create 7.php

---
+++ 7.php
@@ -0,0 +1,14 @@
+<?php
+function count_code($str) {
+  $count = mb_strpos($str,"code");
+  if ($count >= 0) {
+    echo ($count+1)."番目に該当の文字あり\n";
+  } else {
+    echo "該当の文字なし\n";
+  }
+}
+
+count_code("acodecount");
+count_code("codecount");
+count_code("wherecodefrom");
+?>
