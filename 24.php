a9fc988d8070834f2ec1859674f52d60846ed299
Yuki03280322 <my-op.cpace@outlook.jp>
2021-04-29 09:29:37 +0900

    Update 24.php

--- 24.php
+++ 24.php
@@ -19,4 +19,33 @@ lone_sum(5,5,3);
 lone_sum(2,3,2);
 lone_sum(3,4,4);
 lone_sum(3,3,3);
+
+function lone_sum2($a, $b, $c) {
+  $array = array($a, $b, $c);
+  $uniq_nums = [];
+  foreach($array as $nums) {
+    $count = 0;
+    foreach($array as $num) {
+      if ($num == $nums) {
+        $count += 1;
+      }
+    }
+    if ($count < 2) {
+      array_push($uniq_nums, $nums);
+    }
+  }
+  $sum = 0;
+  foreach($uniq_nums as $uniq_nums) {
+    $sum += $uniq_nums;
+  }
+  echo $sum. "\n";
+}
+
+lone_sum2(4,5,6);
+lone_sum2(5,5,3);
+lone_sum2(2,3,2);
+lone_sum2(3,4,4);
+lone_sum2(3,3,3);
+
+
 ?>
