524af8969eb36e6020ce046cbf6f0ceb3a775cea
Yuki03280322 <my-op.cpace@outlook.jp>
2021-05-03 12:26:44 +0900

    Update 28.php

--- 28.php
+++ 28.php
@@ -2,12 +2,12 @@
 function near_ten($num) {
   $hyaku = ($num / 100) % 10;
   $zyu = ($num / 10) % 10;
-  $iti = ($num / 1) % 10;
+  $iti = $num % 10;
   $result = $hyaku + $zyu + $iti;
-  $result_iti = ($result / 1) % 10;
+  $result_iti = $result % 10;
   if($result_iti <= 2 || $result_iti >= 8) {
     echo "True\n";
-  } elseif($result_iti == 3 || $result_iti == 4) {
+  } elseif($result_iti <= 5) {
     echo "10の倍数との差は". $result_iti. "です\n";
   } else {
     echo "10の倍数との差は". (10 - $result_iti). "です\n";
