8977474db028d7d9523a0d1e195d7d6b92d98ff2
Yuki03280322 <my-op.cpace@outlook.jp>
2021-05-21 00:47:59 +0900

    Update 42.php

--- 42.php
+++ 42.php
@@ -7,7 +7,7 @@ $sale = $array[1];
 $pay = $price;
 while($price > 0) {
   $sale_price = floor($price * $sale / 100);
-  if($sale_price == 0) {
+  if($sale_price < 1) {
     break;
   }
   $price -= $sale_price;
